6cc53cfac8eed4141674f58c231280f21e3c9795
OliverMengich <kemeioliver2000@gmail.com>
2022-12-10 19:55:26 +0300

    Incorporates testing

---
+++ test/api.test.js
@@ -0,0 +1,31 @@
+const app = require('../index.js');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+chai.should();
+describe("BID API tests",(done)=>{
+
+    describe("Gets products in Database",(done)=>{
+        it("it should GET Products",(done)=>{
+            let requestBody = {
+                query: `
+                    query{
+                        products{
+                            title,
+                            price
+                        }
+                    }
+                `
+            }
+            chai.request(app)
+            .get("/graphql")
+            .send(requestBody)
+            .end((err,res)=>{
+                // console.log(res.body.data);
+                res.should.have.status(200);
+                res.body.data.should.have.property("products")
+                done();
+            })
+        })
+    })
+})
